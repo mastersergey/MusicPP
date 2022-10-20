36573ee28e26558c9f753a9a8737d24afc154db3
Sergey <belyaev.sergey2000@mail.ru>
2022-10-20 16:40:40 +0300

    add loading on artist page

--- src/pages/artist-page/artist-page.tsx
+++ src/pages/artist-page/artist-page.tsx
@@ -8,26 +8,7 @@ import YT from '../../api/youtube-api';
 import { Flexbox } from '../../components/styled/flexbox';
 import StyledButton from '../../components/styled/styled-button';
 
-const ResponiveContainer = styled.div`
-  height: 110vh;
-  @media (max-height: 1100px) {
-    height: 120vh;
-  }
-  @media (max-height: 1000px) {
-    height: 130vh;
-  }
-  @media (max-height: 900px) {
-    height: 150vh;
-  }
-  @media (max-height: 800px) {
-    height: 165vh;
-  }
-  @media (max-height: 700px) {
-    height: 175vh;
-  }
-`;
-
-const HeaderWrapper = styled(Flexbox)`
+const BannerWrapper = styled(Flexbox)`
   background-image: url(${(props) => props.theme});
   background-repeat: no-repeat;
   background-position: center;
@@ -35,7 +16,7 @@ const HeaderWrapper = styled(Flexbox)`
   width: 100%;
 `;
 
-const HeaderTitle = styled.h1`
+const ArtistTitle = styled.h1`
   font-size: 80px;
   @media (max-width: 930px) {
     font-size: 70px;
@@ -86,81 +67,91 @@ function ArtistPage() {
   useEffect(() => {
     YT.GET.ARTIST_INFO(params.artistId).then((info) => setPageInfo(info));
     YT.GET.ALBUMS(params.artistId).then((albums) => setAlbumsList(albums));
-  }, [params]);
+  }, [params.artistId]);
 
   useEffect(() => {
-    YT.GET.SONGS(pageInfo.title, 5).then((songs) => setSongsList(songs));
+    if (typeof pageInfo.title !== 'undefined') {
+      YT.GET.SONGS(pageInfo.title, 5).then((songs) => setSongsList(songs));
+    }
   }, [pageInfo]);
 
   return (
-    <ResponiveContainer>
-      {pageInfo.banner && (
-        <HeaderWrapper justify="flex-end" direction="column" theme={pageInfo.banner} />
-      )}
-      <HeaderTitle>{pageInfo.title}</HeaderTitle>
-      <p>{pageInfo.subscribersCount} subscribers</p>
-      <StatusWrapper>
-        <a style={{ marginRight: '30px' }}>
-          <svg
-            width="56"
-            height="56"
-            viewBox="0 0 56 56"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle cx="28" cy="28" r="28" fill="#1DB954" />
-            <path d="M21 38.5L39.375 28L21 17.5V38.5Z" fill="white" />
-          </svg>
-        </a>
-        <div>
-          <FollowButton>FOLLOWING</FollowButton>
-        </div>
-      </StatusWrapper>
-      <div className="artist__songs">
-        <div className="artist__songs-popular">
-          <h2>Popular</h2>
-          <div className="artist__songs-popular-top">
-            {songsList.map((song: TContentItem, index) => (
-              <div className="artist__songs-popular-top-item" key={song.id}>
-                <div className="artist__songs-popular-top-item-icon">
-                  <p>{index + 1}</p>
-                  <img alt="p" src={song.snippet.thumbnails.high.url}></img>
-                </div>
-                <div className="artist__songs-popular-top-item-inner">
-                  <span className="track">{song.snippet.title}</span>
-                  <span className="timecode">03:33</span>
-                </div>
-              </div>
-            ))}
-          </div>
-          <a>SEE MORE</a>
-        </div>
-        <div className="artist__songs-liked">
-          <h2>Liked Songs</h2>
-          <div className="artist__songs-liked-inner">
-            <img src="/assets/albom.jpg" alt="p" />
+    <div>
+      {songsList.length !== 0 && (
+        <>
+          {pageInfo.banner && (
+            <BannerWrapper
+              justify="flex-end"
+              direction="column"
+              theme={pageInfo.banner}
+            />
+          )}
+          <ArtistTitle>{pageInfo.title}</ArtistTitle>
+          <p>{pageInfo.subscribersCount} subscribers</p>
+          <StatusWrapper>
+            <a style={{ marginRight: '30px' }}>
+              <svg
+                width="56"
+                height="56"
+                viewBox="0 0 56 56"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="28" cy="28" r="28" fill="#1DB954" />
+                <path d="M21 38.5L39.375 28L21 17.5V38.5Z" fill="white" />
+              </svg>
+            </a>
             <div>
-              <h2>Youve liked 4 songs</h2>
-              <p>by Twenty One Pilots</p>
+              <FollowButton>FOLLOWING</FollowButton>
             </div>
-          </div>
-        </div>
-      </div>
-      <div className="artist__alboms">
-        <h2 className="artist__alboms-title">Popular releases</h2>
-        <div className="artist__alboms-list">
-          {albumsList.map((album: TContentItem) => (
-            <div key={album.id} className="artist__alboms-list-item">
-              <img src={album.snippet.thumbnails.high.url} alt="p" />
-              <div>
-                <p>{album.snippet.title}</p>
-                <p>2015</p>
+          </StatusWrapper>
+          <div className="artist__songs">
+            <div className="artist__songs-popular">
+              <h2>Popular</h2>
+              <div className="artist__songs-popular-top">
+                {songsList.map((song: TContentItem, index) => (
+                  <div className="artist__songs-popular-top-item" key={song.id}>
+                    <div className="artist__songs-popular-top-item-icon">
+                      <p>{index + 1}</p>
+                      <img alt="p" src={song.snippet.thumbnails.high.url}></img>
+                    </div>
+                    <div className="artist__songs-popular-top-item-inner">
+                      <span className="track">{song.snippet.title}</span>
+                      <span className="timecode">03:33</span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <a>SEE MORE</a>
+            </div>
+            <div className="artist__songs-liked">
+              <h2>Liked Songs</h2>
+              <div className="artist__songs-liked-inner">
+                <img src="/assets/albom.jpg" alt="p" />
+                <div>
+                  <h2>Youve liked 4 songs</h2>
+                  <p>by Twenty One Pilots</p>
+                </div>
               </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </ResponiveContainer>
+          </div>
+          <div className="artist__alboms">
+            <h2 className="artist__alboms-title">Popular releases</h2>
+            <div className="artist__alboms-list">
+              {albumsList.map((album: TContentItem) => (
+                <div key={album.id} className="artist__alboms-list-item">
+                  <img src={album.snippet.thumbnails.high.url} alt="p" />
+                  <div>
+                    <p>{album.snippet.title}</p>
+                    <p>2015</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
