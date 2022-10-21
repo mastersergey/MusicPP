a9be3f5bc2d49687011be79efdc5c63abc4134af
Sergey <belyaev.sergey2000@mail.ru>
2022-10-21 18:08:48 +0300

    some refactor

---
+++ src/components/artist-songs/styles.ts
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+import { Flexbox } from '../styled/flexbox';
+
+const ArtistSongsItemWrapper = styled(Flexbox)`
+  font-weight: 500;
+  margin-bottom: 10px;
+  width: 100%;
+  transition: 0.1s linear;
+  &:hover {
+    color: green;
+  }
+`;
+
+const ArtistSongsItemIcon = styled.img`
+  margin: 0 10px;
+  width: 70px;
+  height: 70px;
+`;
+
+const TopWrapper = styled.div`
+  padding-left: 50px;
+  @media (max-width: 375px) {
+    display: none;
+  }
+`;
+const SongsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 65% 35%;
+  padding: 10px 50px;
+  margin-bottom: 20px;
+  @media (max-width: 1320px) {
+    grid-template-columns: 90% 10%;
+  }
+`;
+
+const LikedSongsWrapper = styled.div`
+  padding-left: 50px;
+  h2 {
+    margin-bottom: 20px;
+  }
+`;
+const LikedSongsInner = styled(Flexbox)`
+  img {
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+    margin-right: 20px;
+  }
+`;
+
+export {
+  ArtistSongsItemIcon,
+  ArtistSongsItemWrapper,
+  LikedSongsInner,
+  LikedSongsWrapper,
+  SongsWrapper,
+  TopWrapper,
+};
